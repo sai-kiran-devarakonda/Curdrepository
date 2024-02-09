c88420da76c649f285f27609e41df83db973a2c2
sai-kiran-devarakonda <dsaikiran9966@gmail.com>
2024-02-09 15:39:02 +0530

    i changed print name

--- demoproject/src/main/java/com/example/demoproject/DemoprojectApplication.java
+++ demoproject/src/main/java/com/example/demoproject/DemoprojectApplication.java
@@ -8,7 +8,7 @@ public class DemoprojectApplication {
 
 	public static void main(String[] args) {
 		
-		System.out.println("kiran");
+		System.out.println("kirandevarakonda");
 		SpringApplication.run(DemoprojectApplication.class, args);
 	}
 
