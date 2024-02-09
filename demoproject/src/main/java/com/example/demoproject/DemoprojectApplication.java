a7d318259f6d0adcc3a56513adf1876e857b4cbf
sai-kiran-devarakonda <dsaikiran9966@gmail.com>
2024-02-09 15:31:24 +0530

    first commit

---
+++ demoproject/src/main/java/com/example/demoproject/DemoprojectApplication.java
@@ -0,0 +1,15 @@
+package com.example.demoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoprojectApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("kiran");
+		SpringApplication.run(DemoprojectApplication.class, args);
+	}
+
+}
